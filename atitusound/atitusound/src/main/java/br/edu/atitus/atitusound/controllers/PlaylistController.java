9961c698c0c6f447111b45b7587f1c617349f76d
jmdambros <jmcdambros@gmail.com>
2023-10-30 21:31:34 -0300

    Atividade Avaliativa Finalizada
    Parte faltante playlists implementada

---
+++ atitusound/atitusound/src/main/java/br/edu/atitus/atitusound/controllers/PlaylistController.java
@@ -0,0 +1,56 @@
+package br.edu.atitus.atitusound.controllers;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.atitus.atitusound.dtos.ArtistDTO;
+import br.edu.atitus.atitusound.dtos.PlaylistDTO;
+import br.edu.atitus.atitusound.entities.ArtistEntity;
+import br.edu.atitus.atitusound.entities.PlaylistEntity;
+import br.edu.atitus.atitusound.services.ArtistService;
+import br.edu.atitus.atitusound.services.GenericService;
+import br.edu.atitus.atitusound.services.PlaylistService;
+
+@RestController
+@RequestMapping("/playlists")
+public class PlaylistController extends GenericController<PlaylistEntity,PlaylistDTO>{
+	private final PlaylistService playlistService;
+
+	public PlaylistController(PlaylistService playlistService) {
+		super();
+		this.playlistService = playlistService;
+	}
+
+	
+	protected PlaylistEntity convertDTO2Entity(PlaylistDTO dto) {
+		PlaylistEntity entidade = new PlaylistEntity();
+		BeanUtils.copyProperties(dto, entidade);
+		return entidade;
+	}
+
+
+	@Override
+	public GenericService<PlaylistEntity> getService() {
+		return playlistService;
+	}
+	
+}
---
+++ atitusound/atitusound/src/main/java/br/edu/atitus/atitusound/dtos/PlaylistDTO.java
@@ -0,0 +1,24 @@
+package br.edu.atitus.atitusound.dtos;
+
+
+public class PlaylistDTO {
+	private String name;
+	private boolean public_share;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isPublic_share() {
+		return public_share;
+	}
+	public void setPublic_share(boolean public_share) {
+		this.public_share = public_share;
+
+	}
+
+	
+
+}
